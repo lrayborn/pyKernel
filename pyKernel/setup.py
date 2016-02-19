bedaf766028f16a2b27c511412cf051ca89f0a70
Lance Rayborn <lancerayborn@lraybornmac.eas.albany.edu>
2016-02-19 11:41:13 -0500

    Add initial pyKernel package

---
+++ pyKernel/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='kernel',
+      version='0.1',
+      description='Package to calculate feedbacks using radiative kernels',
+      author='Brian E. J. Rose & Lance Rayborn',
+      author_email='brose@albany.edu',
+      license='MIT',
+      packages=['kernel'],
+      zip_safe=False)
+
